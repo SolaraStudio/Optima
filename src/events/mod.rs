ef384999faa8fda14d5267c1a26cd5189d56b479
Frostre_ <n9043395@gmail.com>
2026-08-28 21:55:59 +0200

    Add gesture, focus, scroll, resize, click, and pointer events
    Added new event types and extended EventQueue functionality.

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,20 +1,35 @@
 pub mod mouse;
 pub mod keyboard;
 pub mod touch;
+pub mod gesture;
+pub mod focus;
+pub mod scroll;
+pub mod resize;
+pub mod click;
+pub mod pointer;
+pub mod mousedown;
+pub mod mouseup;
+pub mod mousemove;
+pub mod keydown;
+pub mod keyup;
 
 use std::collections::VecDeque;
-
-pub struct EventQueue {
-    events: VecDeque<Event>,
-}
 
 #[derive(Debug, Clone)]
 pub enum Event {
     Mouse(mouse::MouseEvent),
     Keyboard(keyboard::KeyEvent),
     Touch(touch::TouchEvent),
-    Resize { width: u32, height: u32 },
-    Scroll { delta_x: f32, delta_y: f32 },
+    Gesture(gesture::GestureEvent),
+    Focus(focus::FocusEvent),
+    Scroll(scroll::ScrollEvent),
+    Resize(resize::ResizeEvent),
+    Click(click::ClickEvent),
+    Pointer(pointer::PointerEvent),
+}
+
+pub struct EventQueue {
+    events: VecDeque<Event>,
 }
 
 impl EventQueue {
@@ -32,11 +47,38 @@ impl EventQueue {
         self.events.pop_front()
     }
 
+    pub fn peek(&self) -> Option<&Event> {
+        self.events.front()
+    }
+
     pub fn is_empty(&self) -> bool {
         self.events.is_empty()
     }
 
+    pub fn len(&self) -> usize {
+        self.events.len()
+    }
+
     pub fn clear(&mut self) {
         self.events.clear();
+    }
+
+    pub fn extend(&mut self, other: &mut EventQueue) {
+        while let Some(event) = other.pop() {
+            self.push(event);
+        }
+    }
+
+    pub fn filter<F>(&mut self, predicate: F)
+    where
+        F: Fn(&Event) -> bool,
+    {
+        self.events.retain(predicate);
+    }
+}
+
+impl Default for EventQueue {
+    fn default() -> Self {
+        Self::new()
     }
 }

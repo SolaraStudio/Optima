23d4420e8ac283f9d29fc6a86f33e2d18940e568
Frostre_ <n9043395@gmail.com>
2026-08-28 21:48:43 +0200

    Enhance Decoder with video/audio properties and methods
    Refactor Decoder struct to include video dimensions and audio properties. Update initialization methods for video and audio decoders to accept additional parameters.

--- src/media/decoder/decoder.rs
+++ src/media/decoder/decoder.rs
@@ -1,13 +1,17 @@
 use jni::objects::{JObject, JClass};
 use jni::JNIEnv;
-use crate::media::VideoFrame;
-use crate::media::AudioFrame;
+use crate::media::{VideoFrame, AudioFrame};
+use crate::media::codec::Codec;
 
 pub struct Decoder {
-    video_codec: Option<String>,
-    audio_codec: Option<String>,
+    video_codec: Option<Codec>,
+    audio_codec: Option<Codec>,
     video_handle: Option<jlong>,
     audio_handle: Option<jlong>,
+    video_width: u32,
+    video_height: u32,
+    audio_sample_rate: u32,
+    audio_channels: u8,
 }
 
 impl Decoder {
@@ -17,17 +21,44 @@ impl Decoder {
             audio_codec: None,
             video_handle: None,
             audio_handle: None,
+            video_width: 0,
+            video_height: 0,
+            audio_sample_rate: 0,
+            audio_channels: 0,
         }
     }
 
-    pub fn init_video_decoder(&mut self, env: &mut JNIEnv, mime_type: &str) -> bool {
+    pub fn init_video_decoder(&mut self, env: &mut JNIEnv, codec: Codec, width: u32, height: u32) -> bool {
         let class = env.find_class("org/optima/MediaCodecDecoder").unwrap();
-        let method = env.get_static_method_id(class, "create", "(Ljava/lang/String;)J").unwrap();
-        let mime_jstring = env.new_string(mime_type).unwrap();
-        let result = env.call_static_method(class, method, &[(&mime_jstring).into()]).unwrap();
+        let method = env.get_static_method_id(class, "create", "(Ljava/lang/String;II)J").unwrap();
+        let mime_jstring = env.new_string(codec.mime_type()).unwrap();
+        let result = env.call_static_method(class, method, &[
+            (&mime_jstring).into(),
+            width.into(),
+            height.into(),
+        ]).unwrap();
         let ptr = result.j().unwrap();
         self.video_handle = Some(ptr);
-        self.video_codec = Some(mime_type.to_string());
+        self.video_codec = Some(codec);
+        self.video_width = width;
+        self.video_height = height;
+        true
+    }
+
+    pub fn init_audio_decoder(&mut self, env: &mut JNIEnv, codec: Codec, sample_rate: u32, channels: u8) -> bool {
+        let class = env.find_class("org/optima/MediaCodecDecoder").unwrap();
+        let method = env.get_static_method_id(class, "createAudio", "(Ljava/lang/String;II)J").unwrap();
+        let mime_jstring = env.new_string(codec.mime_type()).unwrap();
+        let result = env.call_static_method(class, method, &[
+            (&mime_jstring).into(),
+            sample_rate.into(),
+            channels.into(),
+        ]).unwrap();
+        let ptr = result.j().unwrap();
+        self.audio_handle = Some(ptr);
+        self.audio_codec = Some(codec);
+        self.audio_sample_rate = sample_rate;
+        self.audio_channels = channels;
         true
     }
 
@@ -40,14 +71,75 @@ impl Decoder {
         let output = result.l().unwrap();
         let buffer = env.convert_byte_array(output).unwrap();
         Some(VideoFrame {
-            width: 640,
-            height: 480,
+            width: self.video_width,
+            height: self.video_height,
             data: buffer,
             pts: 0,
+            dts: 0,
+            duration: 0,
+            is_keyframe: true,
         })
     }
 
-    pub fn decode_audio_frame(&self, _data: &[u8]) -> Option<AudioFrame> {
+    pub fn decode_video_frame_with_pts(&self, env: &mut JNIEnv, data: &[u8], pts: u64) -> Option<VideoFrame> {
+        let ptr = self.video_handle?;
+        let class = env.find_class("org/optima/MediaCodecDecoder").unwrap();
+        let method = env.get_method_id(class, "decodeFrame", "([B)[B").unwrap();
+        let byte_array = env.byte_array_from_slice(data).unwrap();
+        let result = env.call_method(ptr, method, &[(&byte_array).into()]).unwrap();
+        let output = result.l().unwrap();
+        let buffer = env.convert_byte_array(output).unwrap();
+        Some(VideoFrame {
+            width: self.video_width,
+            height: self.video_height,
+            data: buffer,
+            pts,
+            dts: 0,
+            duration: 0,
+            is_keyframe: true,
+        })
+    }
+
+    pub fn decode_audio_frame(&self, data: &[u8]) -> Option<AudioFrame> {
+        // Placeholder - implement with ffmpeg or MediaCodec
         None
+    }
+
+    pub fn has_video(&self) -> bool {
+        self.video_handle.is_some()
+    }
+
+    pub fn has_audio(&self) -> bool {
+        self.audio_handle.is_some()
+    }
+
+    pub fn get_video_codec(&self) -> Option<Codec> {
+        self.video_codec
+    }
+
+    pub fn get_audio_codec(&self) -> Option<Codec> {
+        self.audio_codec
+    }
+
+    pub fn get_video_width(&self) -> u32 {
+        self.video_width
+    }
+
+    pub fn get_video_height(&self) -> u32 {
+        self.video_height
+    }
+
+    pub fn get_audio_sample_rate(&self) -> u32 {
+        self.audio_sample_rate
+    }
+
+    pub fn get_audio_channels(&self) -> u8 {
+        self.audio_channels
+    }
+}
+
+impl Default for Decoder {
+    fn default() -> Self {
+        Self::new()
     }
 }

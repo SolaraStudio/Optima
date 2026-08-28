b8e0fc879006b4b0dc01ee15c298c4a83cc1392a
Frostre_ <n9043395@gmail.com>
2026-08-28 16:14:15 +0200

    Refactor WebSocketClient for thread safety and features
    Refactor WebSocketClient to use Arc and Mutex for thread safety. Enhance connection handling and add methods for receiving binary data.

--- src/net/websocket/websocket.rs
+++ src/net/websocket/websocket.rs
@@ -1,57 +1,130 @@
 use tungstenite::{connect, Message};
 use tungstenite::stream::MaybeTlsStream;
 use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
 
 pub struct WebSocketClient {
-    stream: Option<WebSocket<MaybeTlsStream<TcpStream>>>,
+    stream: Arc<Mutex<Option<WebSocketStream>>>,
+    url: String,
+    connected: bool,
 }
+
+type WebSocketStream = tungstenite::WebSocket<MaybeTlsStream<TcpStream>>;
 
 impl WebSocketClient {
     pub fn new() -> Self {
-        Self { stream: None }
+        Self {
+            stream: Arc::new(Mutex::new(None)),
+            url: String::new(),
+            connected: false,
+        }
     }
 
-    pub fn connect(&mut self, url: &str) -> bool {
+    pub fn connect(&mut self, url: &str) -> Result<(), tungstenite::Error> {
         match connect(url) {
             Ok((stream, _)) => {
-                self.stream = Some(stream);
-                true
+                *self.stream.lock().unwrap() = Some(stream);
+                self.url = url.to_string();
+                self.connected = true;
+                Ok(())
             }
-            Err(_) => false,
+            Err(e) => Err(e),
         }
     }
 
-    pub fn send(&mut self, message: &str) -> bool {
-        if let Some(stream) = &mut self.stream {
-            stream.send(Message::Text(message.to_string())).is_ok()
+    pub fn send(&mut self, message: &str) -> Result<(), tungstenite::Error> {
+        let mut guard = self.stream.lock().unwrap();
+        if let Some(stream) = guard.as_mut() {
+            stream.send(Message::Text(message.to_string()))?;
+            Ok(())
         } else {
-            false
+            Err(tungstenite::Error::ConnectionClosed)
         }
     }
 
-    pub fn send_binary(&mut self, data: &[u8]) -> bool {
-        if let Some(stream) = &mut self.stream {
-            stream.send(Message::Binary(data.to_vec())).is_ok()
+    pub fn send_binary(&mut self, data: &[u8]) -> Result<(), tungstenite::Error> {
+        let mut guard = self.stream.lock().unwrap();
+        if let Some(stream) = guard.as_mut() {
+            stream.send(Message::Binary(data.to_vec()))?;
+            Ok(())
         } else {
-            false
+            Err(tungstenite::Error::ConnectionClosed)
         }
     }
 
-    pub fn receive(&mut self) -> Option<String> {
-        if let Some(stream) = &mut self.stream {
-            if let Ok(msg) = stream.read() {
-                if msg.is_text() {
-                    return Some(msg.into_text().unwrap());
+    pub fn receive(&mut self) -> Result<Option<String>, tungstenite::Error> {
+        let mut guard = self.stream.lock().unwrap();
+        if let Some(stream) = guard.as_mut() {
+            match stream.read() {
+                Ok(msg) => {
+                    if msg.is_text() {
+                        Ok(Some(msg.into_text().unwrap()))
+                    } else if msg.is_binary() {
+                        Ok(Some(format!("{:?}", msg.into_data())))
+                    } else if msg.is_close() {
+                        self.connected = false;
+                        Ok(None)
+                    } else {
+                        Ok(None)
+                    }
+                }
+                Err(e) => {
+                    self.connected = false;
+                    Err(e)
                 }
             }
+        } else {
+            Ok(None)
         }
-        None
     }
 
-    pub fn close(&mut self) {
-        if let Some(stream) = &mut self.stream {
-            let _ = stream.close(None);
+    pub fn receive_binary(&mut self) -> Result<Option<Vec<u8>>, tungstenite::Error> {
+        let mut guard = self.stream.lock().unwrap();
+        if let Some(stream) = guard.as_mut() {
+            match stream.read() {
+                Ok(msg) => {
+                    if msg.is_binary() {
+                        Ok(Some(msg.into_data()))
+                    } else if msg.is_text() {
+                        Ok(Some(msg.into_text().unwrap().into_bytes()))
+                    } else if msg.is_close() {
+                        self.connected = false;
+                        Ok(None)
+                    } else {
+                        Ok(None)
+                    }
+                }
+                Err(e) => {
+                    self.connected = false;
+                    Err(e)
+                }
+            }
+        } else {
+            Ok(None)
         }
-        self.stream = None;
+    }
+
+    pub fn close(&mut self) -> Result<(), tungstenite::Error> {
+        let mut guard = self.stream.lock().unwrap();
+        if let Some(stream) = guard.as_mut() {
+            stream.close(None)?;
+            self.connected = false;
+        }
+        *guard = None;
+        Ok(())
+    }
+
+    pub fn is_connected(&self) -> bool {
+        self.connected
+    }
+
+    pub fn get_url(&self) -> &str {
+        &self.url
+    }
+}
+
+impl Default for WebSocketClient {
+    fn default() -> Self {
+        Self::new()
     }
 }

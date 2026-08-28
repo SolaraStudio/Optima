6dc0568840daf9111cb19a530b5fd19135995fd6
Frostre_ <n9043395@gmail.com>
2026-08-28 12:25:51 +0200

    Refactor BoxModel struct for improved readability

--- src/layout/box_model.rs
+++ src/layout/box_model.rs
@@ -1,2 +1,15 @@
-//! Box model
-pub struct BoxModel { pub margin: f32, pub padding: f32, pub border: f32 }
+pub struct BoxModel {
+    pub margin: f32,
+    pub padding: f32,
+    pub border: f32,
+}
+
+impl BoxModel {
+    pub fn new() -> Self {
+        Self {
+            margin: 0.0,
+            padding: 0.0,
+            border: 0.0,
+        }
+    }
+}

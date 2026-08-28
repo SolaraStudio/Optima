365dbb3bc66064b588794262f26dad672eb268d4
Frostre_ <n9043395@gmail.com>
2026-08-28 14:10:54 +0200

    Implement EventQueue for handling events
    Added an event queue structure to manage different types of events.

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,4 +1,42 @@
-//! Event system (mouse, keyboard, touch)
 pub mod mouse;
 pub mod keyboard;
 pub mod touch;
+
+use std::collections::VecDeque;
+
+pub struct EventQueue {
+    events: VecDeque<Event>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Event {
+    Mouse(mouse::MouseEvent),
+    Keyboard(keyboard::KeyEvent),
+    Touch(touch::TouchEvent),
+    Resize { width: u32, height: u32 },
+    Scroll { delta_x: f32, delta_y: f32 },
+}
+
+impl EventQueue {
+    pub fn new() -> Self {
+        Self {
+            events: VecDeque::new(),
+        }
+    }
+
+    pub fn push(&mut self, event: Event) {
+        self.events.push_back(event);
+    }
+
+    pub fn pop(&mut self) -> Option<Event> {
+        self.events.pop_front()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.events.is_empty()
+    }
+
+    pub fn clear(&mut self) {
+        self.events.clear();
+    }
+}

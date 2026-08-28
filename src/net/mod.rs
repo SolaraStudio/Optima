cb60d0ade10b3e3e631a463c0bd4c3ae42d247d0
Frostre_ <n9043395@gmail.com>
2026-08-28 15:47:32 +0200

    Add new networking modules for HTTPS and DNS

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,5 +1,18 @@
-//! Networking (HTTP, WebSocket)
 pub mod http;
+pub mod https;
 pub mod websocket;
 pub mod cache;
 pub mod cookie;
+pub mod dns;
+pub mod fetch;
+pub mod header;
+pub mod proxy;
+pub mod redirect;
+pub mod resource;
+pub mod retry;
+pub mod body;
+
+pub use http::HttpClient;
+pub use websocket::WebSocketClient;
+pub use cache::Cache;
+pub use cookie::CookieManager;

0f4de61898f229883da04a7911af1687ba317ad4
Frostre_ <n9043395@gmail.com>
2026-08-28 12:27:06 +0200

    Refactor KeyEvent to include KeyAction enum

--- src/events/keyboard.rs
+++ src/events/keyboard.rs
@@ -1,2 +1,21 @@
-//! Keyboard events
-pub struct KeyEvent { pub key: String, pub code: u16, pub pressed: bool }
+pub struct KeyEvent {
+    pub key: String,
+    pub code: u16,
+    pub action: KeyAction,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum KeyAction {
+    Down,
+    Up,
+}
+
+impl KeyEvent {
+    pub fn new(key: &str, code: u16, action: KeyAction) -> Self {
+        Self {
+            key: key.to_string(),
+            code,
+            action,
+        }
+    }
+}

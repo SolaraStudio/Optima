9aef91a3bae16a27ba89c2014a5be569b458405a
Frostre_ <n9043395@gmail.com>
2026-08-28 12:23:51 +0200

    Refactor ComputedStyle to use Option types

--- src/css/computed.rs
+++ src/css/computed.rs
@@ -1,2 +1,29 @@
-//! Computed styles
-pub struct ComputedStyle { pub color: String, pub background: String }
+use std::collections::HashMap;
+
+pub struct ComputedStyle {
+    pub color: Option<String>,
+    pub background: Option<String>,
+    pub font_size: Option<f32>,
+    pub width: Option<f32>,
+    pub height: Option<f32>,
+}
+
+impl ComputedStyle {
+    pub fn new() -> Self {
+        Self {
+            color: None,
+            background: None,
+            font_size: None,
+            width: None,
+            height: None,
+        }
+    }
+
+    pub fn merge(&mut self, other: ComputedStyle) {
+        if let Some(v) = other.color { self.color = Some(v); }
+        if let Some(v) = other.background { self.background = Some(v); }
+        if let Some(v) = other.font_size { self.font_size = Some(v); }
+        if let Some(v) = other.width { self.width = Some(v); }
+        if let Some(v) = other.height { self.height = Some(v); }
+    }
+}

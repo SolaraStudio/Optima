c2bbdfb8ce8521a503c004542630e4b9a40d3e00
Frostre_ <n9043395@gmail.com>
2026-09-01 20:41:13 +0200

    Integrate audio engine and video decoder into MediaPipeline
    Added audio engine and video decoder to MediaPipeline.

--- src/media/mod.rs
+++ src/media/mod.rs
@@ -30,6 +30,8 @@ pub struct MediaPipeline {
     pub has_audio: bool,
     pub playback_state: PlaybackState,
     pub sync_controller: SyncController,
+    pub audio_engine: AudioEngine,
+    pub video_decoder: decoder::Decoder,
 }
 
 impl MediaPipeline {
@@ -42,19 +44,45 @@ impl MediaPipeline {
             has_audio: false,
             playback_state: PlaybackState::Idle,
             sync_controller: SyncController::new(),
+            audio_engine: AudioEngine::new(),
+            video_decoder: decoder::Decoder::new(),
         }
+    }
+
+    pub fn load_url(&mut self, url: &str) -> Result<(), String> {
+        // Try to load as video
+        if let Ok(()) = self.video_decoder.open(url) {
+            self.has_video = true;
+            if let Some(duration) = self.video_decoder.get_duration() {
+                self.duration = duration;
+            }
+        }
+        // Try to load as audio (via demuxer)
+        if let Ok(demuxer) = demuxer::Demuxer::new(url) {
+            self.has_audio = true;
+            if let Some(duration) = demuxer.get_duration() {
+                self.duration = duration;
+            }
+            self.audio_engine.set_demuxer(demuxer);
+        }
+        if !self.has_video && !self.has_audio {
+            return Err("No media tracks found".to_string());
+        }
+        Ok(())
     }
 
     pub fn play(&mut self) {
         self.is_playing = true;
         self.playback_state = PlaybackState::Playing;
         self.sync_controller.start();
+        self.audio_engine.play();
     }
 
     pub fn pause(&mut self) {
         self.is_playing = false;
         self.playback_state = PlaybackState::Paused;
         self.sync_controller.pause();
+        self.audio_engine.pause();
     }
 
     pub fn stop(&mut self) {
@@ -62,24 +90,19 @@ impl MediaPipeline {
         self.position = Duration::from_secs(0);
         self.playback_state = PlaybackState::Stopped;
         self.sync_controller.stop();
+        self.audio_engine.stop();
     }
 
     pub fn seek(&mut self, pos: Duration) {
         self.position = pos;
         self.sync_controller.seek(pos);
+        self.audio_engine.seek(pos);
+        self.video_decoder.seek(pos);
     }
 
     pub fn set_duration(&mut self, dur: Duration) {
         self.duration = dur;
         self.sync_controller.set_duration(dur);
-    }
-
-    pub fn set_has_video(&mut self, has: bool) {
-        self.has_video = has;
-    }
-
-    pub fn set_has_audio(&mut self, has: bool) {
-        self.has_audio = has;
     }
 
     pub fn get_position(&self) -> Duration {
@@ -100,6 +123,20 @@ impl MediaPipeline {
         } else {
             0.0
         }
+    }
+
+    pub fn next_video_frame(&mut self) -> Option<VideoFrame> {
+        if !self.is_playing || !self.has_video {
+            return None;
+        }
+        self.video_decoder.decode_next_frame()
+    }
+
+    pub fn next_audio_samples(&mut self) -> Option<AudioFrame> {
+        if !self.is_playing || !self.has_audio {
+            return None;
+        }
+        self.audio_engine.next_samples()
     }
 }
 

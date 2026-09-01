ca067bc46bbe3e3a326c58c586d0fbcf45e1b21e
Frostre_ <n9043395@gmail.com>
2026-09-01 20:43:45 +0200

    Refactor Codec enum and methods for video only
    Removed unused codec variants and methods related to audio codecs.

--- src/media/codec/codec.rs
+++ src/media/codec/codec.rs
@@ -10,9 +10,6 @@ pub enum Codec {
     Vorbis,
     FLAC,
     PCM,
-    MPEG4,
-    VP8,
-    Theora,
 }
 
 impl Codec {
@@ -22,15 +19,11 @@ impl Codec {
             "video/hevc" => Some(Codec::H265),
             "video/vp9" => Some(Codec::VP9),
             "video/av01" => Some(Codec::AV1),
-            "video/mp4v-es" => Some(Codec::MPEG4),
-            "video/vp8" => Some(Codec::VP8),
-            "video/theora" => Some(Codec::Theora),
             "audio/mp4a-latm" => Some(Codec::AAC),
             "audio/mpeg" => Some(Codec::MP3),
             "audio/opus" => Some(Codec::Opus),
             "audio/vorbis" => Some(Codec::Vorbis),
             "audio/flac" => Some(Codec::FLAC),
-            "audio/pcm" => Some(Codec::PCM),
             _ => None,
         }
     }
@@ -41,9 +34,6 @@ impl Codec {
             Codec::H265 => "video/hevc",
             Codec::VP9 => "video/vp9",
             Codec::AV1 => "video/av01",
-            Codec::MPEG4 => "video/mp4v-es",
-            Codec::VP8 => "video/vp8",
-            Codec::Theora => "video/theora",
             Codec::AAC => "audio/mp4a-latm",
             Codec::MP3 => "audio/mpeg",
             Codec::Opus => "audio/opus",
@@ -54,42 +44,10 @@ impl Codec {
     }
 
     pub fn is_video(&self) -> bool {
-        matches!(self,
-            Codec::H264 | Codec::H265 | Codec::VP9 | Codec::AV1 |
-            Codec::MPEG4 | Codec::VP8 | Codec::Theora
-        )
+        matches!(self, Codec::H264 | Codec::H265 | Codec::VP9 | Codec::AV1)
     }
 
     pub fn is_audio(&self) -> bool {
         !self.is_video()
-    }
-
-    pub fn is_lossless(&self) -> bool {
-        matches!(self, Codec::FLAC | Codec::PCM)
-    }
-
-    pub fn supports_hardware_decoding(&self) -> bool {
-        matches!(self,
-            Codec::H264 | Codec::H265 | Codec::VP9 | Codec::AV1 |
-            Codec::AAC | Codec::MP3 | Codec::Opus
-        )
-    }
-
-    pub fn get_ffmpeg_codec(&self) -> &'static str {
-        match self {
-            Codec::H264 => "h264",
-            Codec::H265 => "hevc",
-            Codec::VP9 => "vp9",
-            Codec::AV1 => "av1",
-            Codec::MPEG4 => "mpeg4",
-            Codec::VP8 => "vp8",
-            Codec::Theora => "theora",
-            Codec::AAC => "aac",
-            Codec::MP3 => "mp3",
-            Codec::Opus => "opus",
-            Codec::Vorbis => "vorbis",
-            Codec::FLAC => "flac",
-            Codec::PCM => "pcm",
-        }
     }
 }

73fec02945e80c9ce3d30bef7db094f8c3da6383
Frostre_ <n9043395@gmail.com>
2026-09-03 10:09:50 +0200

    Downgrade Android library plugin version to 8.13.0

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.library") version "8.14.0" apply false
+    id("com.android.library") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
